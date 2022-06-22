e4577045b987e8f027263db5857a2bbca1286834
Sammith1 <hsammith@gmail.com>
2022-06-22 15:05:04 +0530

    added wight

--- metafic/src/Components/Routetwo.jsx
+++ metafic/src/Components/Routetwo.jsx
@@ -4,14 +4,23 @@ import { useParams, useLocation } from "react-router-dom";
 export const RouteTwo = ({ element }) => {
   // const { type } = useParams();
   const { state } = useLocation().state;
-  console.log(state.name);
+  console.log(state.moves[20].move);
 
   return (
-    <div>
+    <div
+      style={{
+        width: "350px",
+        border: "2px solid red",
+        margin: "auto",
+        textAlign: "center",
+        marginTop: "80px",
+      }}
+    >
       <h1>UserInfo</h1>
       <p>{`Name: ${state.name}`}</p>
       <p>{`Height: ${state.height}`}</p>
       <p>{`Base Experience: ${state.base_experience}`}</p>
+      <p>{`Weight: ${state.weight}`}</p>
     </div>
   );
 };
